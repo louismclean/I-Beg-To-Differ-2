4134b751bc038cf5cda8e891eef24db81dae63f6
Tim Smith <tesmith@gmail.com>
2015-10-17 00:17:39 -0400

    added some things

---
+++ I Beg To Differ/Assets/Scripts/Resource.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resource : MonoBehaviour {
+
+	public int quantity = 0;
+	private TextMesh text;
+
+	// Use this for initialization
+	void Start () {
+		text = this.GetComponent<TextMesh> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = quantity.ToString ();
+	}
+
+	public void add(int amnt){
+		quantity += amnt;
+	}
+
+	public bool canSpend(int amnt){
+		if(quantity>amnt){
+			return true;
+		}
+		return false;
+	}
+
+	public void spend(int amnt){
+		quantity -= amnt;
+	}
+}
---
+++ I Beg To Differ/Assets/Scripts/resourceManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class resourceManager : MonoBehaviour {
+
+	public Resource coins;
+	public Resource wood;
+	public Resource blankets;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	
+	public void getCoin(int amnt){
+		coins.add (amnt);
+	}
+
+	public void getWood(int amnt){
+		wood.add (amnt);
+	}
+
+
+	public void getBlanket(int amnt){
+		blankets.add (amnt);
+	}
+}
+
+
