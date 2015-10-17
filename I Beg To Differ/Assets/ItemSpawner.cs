bb3868c5831bc6f53d4ab142e0594d192036e1bb
Louis McLean <louis.mclean@gmail.com>
2015-10-17 19:04:35 -0400

    Resource spawning take 1

---
+++ I Beg To Differ/Assets/ItemSpawner.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemSpawner : MonoBehaviour {
+
+    public enum ResourceType {Wood, Blanket, Change};
+    public ResourceType resourcetype;
+
+    public ResourcePickup resourcePrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Spawn()
+    {
+        GameObject.Instantiate(resourcePrefab, this.transform.position, this.transform.rotation);
+    }
+
+    bool canSpawn()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(this.transform.position, 0.5f);
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            if (colliders[i].gameObject != gameObject)
+            {
+                m_Grounded = true;
+            }
+        }
+    }
+}
---
+++ I Beg To Differ/Assets/SpawnerManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SpawnerManager : MonoBehaviour {
+
+    public int maxLooseChange = 3;
+    public int maxLooseBlankets = 7;
+    public int maxLooseWoods = 10;
+    List<ItemSpawner> ChangeSpawners;
+    List<ItemSpawner> BlanketSpawners;
+    List<ItemSpawner> WoodsSpawners; 
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+         
+	}
+}
