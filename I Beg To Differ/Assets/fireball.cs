f006ed59f78f5ba81f26d2ca87ff09c12918f331
Tim Smith <tesmith@gmail.com>
2015-10-18 18:31:48 -0400

    lava balls now hurt you

--- I Beg To Differ/Assets/fireball.cs
+++ I Beg To Differ/Assets/fireball.cs
@@ -4,9 +4,12 @@ using System.Collections;
 public class fireball : MonoBehaviour {
 	float m_force = 4f;
 	float ttl = 2.5f;
+	public float damage = 0.25f;
+	HoboExposure exp;
 	// Use this for initialization
 	void Start () {
 		this.GetComponent<Rigidbody2D> ().AddForce(new Vector2 (-200f, -m_force));
+		exp = GameObject.FindGameObjectWithTag ("Player").GetComponent<HoboExposure> ();
 	}
 	
 	// Update is called once per frame
@@ -16,6 +19,15 @@ public class fireball : MonoBehaviour {
 
 			Destroy (this.gameObject);
 		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.gameObject.tag == "Player") {
+			exp.exposure+= damage;
+			Destroy(this.gameObject);
+
+		}
+
 	}
 
 }
