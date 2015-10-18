c18b46c7a0207fc1485835c4c0c5f02d9d1cfb08
Tim Smith <tesmith@gmail.com>
2015-10-17 23:51:35 -0400

    lavalauncher, plus some wood fixes in the world

--- I Beg To Differ/Assets/fireSpawner.cs
+++ I Beg To Differ/Assets/fireSpawner.cs
@@ -5,16 +5,25 @@ public class fireSpawner : MonoBehaviour {
 
 	public GameObject fireball;
 
+	float range = 12f;
+
 	float spawnTime = 1f;
 	float ticker;
 
 	// Use this for initialization
 	void Start () {
-	
+		ticker = spawnTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		ticker -= Time.deltaTime;
+		if (ticker < 0) {
+			ticker = spawnTime;
+
+			Instantiate(fireball,this.transform.position +new Vector3(Random.Range(-range,range),0,0),Quaternion.identity);
+
+
+		}
 	}
 }
---
+++ I Beg To Differ/Assets/fireball.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fireball : MonoBehaviour {
+	float m_force = 4f;
+	float ttl = 2.5f;
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<Rigidbody2D> ().AddForce(new Vector2 (-200f, -m_force));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ttl -= Time.deltaTime;
+		if (ttl < 0) {
+
+			Destroy (this.gameObject);
+		}
+	}
+
+}
