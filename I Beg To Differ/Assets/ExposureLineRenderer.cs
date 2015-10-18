267404f77e80fc6a511bf6132dc7c038940d8094
Louis McLean <louis.mclean@gmail.com>
2015-10-18 18:02:04 -0400

    commitcommit

--- I Beg To Differ/Assets/ExposureLineRenderer.cs
+++ I Beg To Differ/Assets/ExposureLineRenderer.cs
@@ -13,22 +13,30 @@ public class ExposureLineRenderer : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        bglineRenderer.material = new Material (Shader.Find("Particles/Additive"));
+        //bglineRenderer.material = new Material (Shader.Find("Particles/Additive"));
+        //bglineRenderer.material = new Material(Shader.Find("Sprites-Default"));
         bglineRenderer.GetComponent<Renderer>().sortingLayerName = "UI";
-        bglineRenderer.SetColors(Color.gray,Color.gray);
+        bglineRenderer.GetComponent<Renderer>().sortingOrder = 0;
+        //bglineRenderer.SetColors(Color.gray,Color.gray);
 
-        filllineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+        //filllineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+        //filllineRenderer.material = new Material(Shader.Find("Sprites-Default"));
         filllineRenderer.GetComponent<Renderer>().sortingLayerName = "UI";
+        filllineRenderer.GetComponent<Renderer>().sortingOrder = 1;
+
+        bglineRenderer.GetComponent<Renderer>().material.SetColor("_TintColor", new Color(1, 1, 1, 1f));
+        filllineRenderer.GetComponent<Renderer>().material.SetColor("_TintColor", new Color(1, 1, 1, 1f));
         
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        bglineRenderer.SetPosition(0, leftAnchor.position);
+        Vector3 pos = Vector3.Lerp(leftAnchor.position, rightAnchor.position, hExpose.exposure);
+        
+        bglineRenderer.SetPosition(0, pos);
         bglineRenderer.SetPosition(1, rightAnchor.position);
 
         filllineRenderer.SetPosition(0, leftAnchor.position);
-        Vector3 pos = Vector3.Lerp(leftAnchor.position, rightAnchor.position, hExpose.exposure);
         filllineRenderer.SetPosition(1, pos);
 
         filllineRenderer.SetColors(Color.green, Color.blue);
